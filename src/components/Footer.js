e4c02ae33f2989aa965e5309f6720cab615ae7e6
BC-macair022 <bc-macair022@NooraJ.local>
2021-11-09 20:53:25 +0200

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <i class="fas fa-home"></i>
+            <p>add icon</p>
+            <p>search icon</p>
+        </div>
+    );
+};
+
+export default Footer;
