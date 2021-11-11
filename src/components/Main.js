067321f93786065701c8340850c9f6fb6d5428ce
Tero Mäntylä <termanty@gmail.com>
2021-11-11 13:51:19 +0200

    App.css file removed. We use style.css instead

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,8 @@ import React from "react";
 const Main = () => {
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>Dance</h1>
+      <h1>Moves</h1>
       This is the Main component
     </div>
   );
