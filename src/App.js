172757f588bc26cebf8a7cee4aa5f51b1cfa2c96
MariaRosenholm <maria.rosenholm@gmail.com>
2021-11-11 20:47:34 +0200

    JSON data added, footer connected to App.js and React router updated to v.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./style.css";
+import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
