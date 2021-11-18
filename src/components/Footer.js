580a51bd1089307dafe7e9c6db37b096b81ffe44
NooraJumppanen <noora.jumppanen@gmail.com>
2021-11-18 14:48:12 +0200

    added fontawesome packages , added icons to footer and styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faPlusSquare, faSearch } from '@fortawesome/free-solid-svg-icons';
+import {Link} from "react-router-dom";
+
 
 const Footer = () => {
   return (
-    <div>
-      <i className="fas fa-home"></i>
-      <p>add icon</p>
-      <p>search icon</p>
+    <div className="footerwrapper">
+      <Link to="/"><FontAwesomeIcon icon={faHome} className="footericon"/></Link>
+      <Link to="/new"><FontAwesomeIcon icon={faPlusSquare} className="footericon"/></Link>
+      <Link to="/search"><FontAwesomeIcon icon={faSearch} className="footericon"/></Link>
     </div>
   );
 };
