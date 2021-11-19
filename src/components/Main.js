af04106232434bf3aa28381b6e54b252ff07d6d3
Saara Leppis <saara@leppihalme.fi>
2021-11-19 10:11:30 +0200

    aadded sign in to main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,24 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <h1>Dance</h1>
-      <h1>Moves</h1>
-      This is the Main component
+    <div className="main-area">
+      <div className="main-text">
+        <h1 className="h1-dance">Dance</h1>
+        <h1 className="h1-move">Move</h1>
+      </div>
+      <form className="login">
+        <div>
+          <label htmlFor="username">USERNAME</label>
+          <input type="text" name="username" id="username" required></input>
+        </div>
+        <div>
+          <label htmlFor="password">PASSWORD</label>
+          <input type="password" name="password" id="password" required></input>
+        </div>
+        <div className="main-submit">
+          <input id="main-submit" type="submit" value="sign in" />
+        </div>
+      </form>
     </div>
   );
 };
