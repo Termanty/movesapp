ecfaf926062410ad75476fd999276ec6015d5435
NooraJumppanen <noora.jumppanen@gmail.com>
2021-11-26 13:41:22 +0200

    changed EditMove styling to match AddMove, changed h1 to h3 and font weight to normal, took labels away from EditMove, removed drop shadow from search result texts

--- src/components/EditMove.js
+++ src/components/EditMove.js
@@ -1,43 +1,33 @@
-import React from "react";
-
 const EditMove = () => {
   return (
     <div className="editmove">
-      <div className="editmove_header_wrapper">
-        <h1>Edit Move</h1>
+      <div>
+        <h1 className="editmove_header">Edit Move</h1>
       </div>
-      <form className="editmove-wrapper">
-        <label>
+      <form className="editmove_form">
           <input
             type="text"
             placeholder="EDIT move name"
             className="edit_move_input"
+            required
           />
-        </label>
-        <label>
           <input
             type="text"
             placeholder="ADD/EDIT creator name"
             className="edit_move_input"
           />
-        </label>
-        <label>
           <input
-            type="text"
-            placeholder=" ADD/EDIT notes"
-            className="add_note"
-          />
-        </label>
-
-        <label>
+            placeholder="ADD/EDIT notes"
+            className="edit_move_input"
+            maxLength="100"
+            />
           <input
             type="text"
             placeholder="ADD/EDIT instagram link"
-            className="add_note"
+            className="edit_move_input"
           />
-        </label>
-        <div className="save_button">
-          <button type="button">SAVE CHANGES</button>
+        <div>
+          <button type="button" className="saveChangesButton">SAVE CHANGES</button>
         </div>
       </form>
     </div>
