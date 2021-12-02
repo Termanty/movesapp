79283c6ea8d0f5dfecf56238592ee5949b6999d2
NooraJumppanen <noora.jumppanen@gmail.com>
2021-12-02 09:35:04 +0200

    working on addmove function

--- src/components/AddMove.js
+++ src/components/AddMove.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const AddMove = () => {
-  return (
-    <div className="addmove">
+class AddMove extends Component {
+  state = {
+    Move: '',
+    Creator: '',
+    HOX: '',
+    Link: ''
+  }
+  
+  addMoveChangeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  addMoveSubmitHandler = (e) => {
+    fetch('http://localhost:3001/moves', {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(this.state)
+    })
+    .then(function(response) {
+      console.log(response)
+      return response.json();
+    });
+      e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="addmove">
       <h3 className="addMoveHeader">Add Move</h3>
-      <form className="addMoveForm">
-        <input type="text" name="newMoveName" placeholder="Add new MOVE NAME" size="10" required />
-        <input type="text" name="newCreatorName" placeholder="Add CREATOR'S NAME (optional)" />
-        <input type="text" name="newNote" placeholder="Add NOTE (optional)" maxLength="200"/>
-        <input type="text" name="newIGlink" placeholder="Add INSTAGRAM LINK (optional)" />
+      <form className="addMoveForm" onSubmit={this.addMoveSubmitHandler}>
+        <input type="text" name="Move" placeholder="Add new MOVE NAME" size="10" required value={this.state.Move} onChange={this.addMoveChangeHandler}/>
+        <input type="text" name="Creator" placeholder="Add CREATOR'S NAME (optional)" value={this.state.Creator} onChange={this.addMoveChangeHandler}/>
+        <input type="text" name="HOX" placeholder="Add NOTE (optional)" maxLength="200" value={this.state.HOX} onChange={this.addMoveChangeHandler}/>
+        <input type="text" name="Link" placeholder="Add INSTAGRAM LINK (optional)" value={this.state.Link} onChange={this.addMoveChangeHandler}/>
         <button type="submit" className="savebutton">save</button>
       </form>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default AddMove;
