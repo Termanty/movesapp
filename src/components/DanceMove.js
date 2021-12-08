2493584204fbc0b8b9fa76793f9f61144e8cc90c
MariaRosenholm <maria.rosenholm@gmail.com>
2021-12-08 18:23:38 +0200

    DanceMove component styled

--- src/components/DanceMove.js
+++ src/components/DanceMove.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-
-
 
 const DanceMove = ({ dancelist }) => {
   const params = useParams();
   let move = dancelist.find((move) => move.Id === +params.id);
-  console.log("move", move)
+
+  let notes;
+  let text;
+
+  if (!move?.HOX) {
+    notes = "";
+  } else {
+    text = "Notes: ";
+    notes = move?.HOX;
+  }
 
   return (
     <div className="DanceMove">
-      <div id="header"></div>
       <h3>{move?.Move}</h3>
+      <p>by</p>
       <h3>{move?.Creator}</h3>
-      <p>{move?.HOX}</p>
-      <p id="IG">{move?.Link}</p>
-      <Link to={"/edit/" + params.id}>
-
-        <button>edit</button>
-
-      </Link>
+      <div id="notesAndIGWrapper">
+        <p>
+          {text}
+          <span>{notes}</span>
+        </p>
+        <p id="IG">{move?.Link}</p>
+      </div>
     </div>
   );
 };
