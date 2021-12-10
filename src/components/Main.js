cee1bf82a087cdf6e13acf76ae6baad3b1048434
NooraJumppanen <noora.jumppanen@gmail.com>
2021-12-10 16:37:09 +0200

    added logo to main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ReactComponent as Logo} from "../img/UP_logo_light_background.svg";
 
 const Main = () => {
   return (
@@ -11,8 +12,12 @@ const Main = () => {
           Mo<span className="ves">ves</span>
         </h1>
       </div>
+      <div className="logo">
+      <Logo />
+      </div>
     </div>
   );
+  
 };
 
 export default Main;
