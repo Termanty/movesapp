263c8a03e22f8a4440138287b236d9287c8ca688
Saara Leppis <saara@leppihalme.fi>
2021-12-10 19:19:48 +0200

    added insta logo to listitem if link available, moved search input

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons"
 
-const ListItem = ({ move, creator, Id }) => {
+
+const ListItem = ({ move, creator, Id, link }) => {
   return (
     <div className="ListItem">
       <Link to={"/moves/" + Id}>
         <p>
-          {move} by {creator}
+          {move} by {creator} <span className="insta-span">{(link === "" || !link ? "" : <FontAwesomeIcon icon={faInstagram} className="insta" />
+          )}</span>
         </p>
       </Link>
     </div>
