88bf4fded9c0b81cd80cd902a91a36aedcf93f49
Tero Mäntylä <termanty@gmail.com>
2022-01-28 20:48:40 +0200

    Refactor: change App component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Component } from "react";
-import { Routes, Route, useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./style.css";
 import Main from "./components/Main";
 import Login from "./components/Login";
@@ -10,56 +10,36 @@ import Footer from "./components/Footer";
 import DanceMove from "./components/DanceMove";
 import Menu from "./components/Menu";
 
-class App extends Component {
-  state = {
-    moves: [],
-  };
+const App = () => {
+  const [moves, setMoves] = useState([]);
 
-  getAllMoves = () => {
+  const getAllMoves = () => {
     fetch("http://localhost:3001/moves/")
       .then((response) => response.json())
-      .then((moveData) => {
-        this.setState({ moves: moveData });
+      .then((data) => {
+        setMoves(data);
       });
   };
 
-  componentDidMount() {
-    this.getAllMoves();
-  }
+  useEffect(getAllMoves, []);
 
-  render() {
-    return (
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/search"
-            element={<Search dancelist={this.state.moves} />}
-          />
-          <Route
-            path={"/moves/:id"}
-            element={<DanceMove dancelist={this.state.moves} />}
-          />
-          <Route
-            path="/new"
-            element={<AddMove getAllMoves={this.getAllMoves} />}
-          />
-          <Route
-            path="/edit/:id"
-            element={
-              <EditMove
-                dancelist={this.state.moves}
-                getAllMoves={this.getAllMoves}
-              />
-            }
-          />
-        </Routes>
-        <Footer />
-        <Menu />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<Search dancelist={moves} />} />
+        <Route path={"/moves/:id"} element={<DanceMove dancelist={moves} />} />
+        <Route path="/new" element={<AddMove getAllMoves={getAllMoves} />} />
+        <Route
+          path="/edit/:id"
+          element={<EditMove dancelist={moves} getAllMoves={getAllMoves} />}
+        />
+      </Routes>
+      <Footer />
+      <Menu />
+    </div>
+  );
+};
 
 export default App;
