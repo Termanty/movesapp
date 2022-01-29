18936fa4c0123a1ee7219c886b8e5af1d60312ce
MariaRosenholm <maria.rosenholm@gmail.com>
2022-01-29 14:32:57 +0200

    logo change

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,22 @@
 import React from "react";
-import {ReactComponent as Logo} from "../img/UP_logo_light_background.svg";
+import { ReactComponent as Logo } from "../img/UP_logo_light_background.svg";
 
 const Main = () => {
   return (
     <div className="main-area-front">
       <div className="main-text">
         <h1 className="h1-dance">
-          Dan<span className="ce">ce</span>
+          Mov<span className="ce">es</span>
         </h1>
         <h1 className="h1-move">
-          Mo<span className="ves">ves</span>
+          <span className="ves">it</span>
         </h1>
       </div>
       <div className="logo">
-      <Logo />
+        <Logo />
       </div>
     </div>
   );
-  
 };
 
 export default Main;
